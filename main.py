ee046b8d4541bf50d5caabba07672dd327c69d19
Yash-Takhtani <iamyashtakhtani@gmail.com>
2026-09-23 01:34:06 +0530

    added scrolling functionality

--- main.py
+++ main.py
@@ -4,9 +4,31 @@ from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 import time
 import math
+import pyautogui
+import numpy as np
+
+pyautogui.PAUSE = 0
+pyautogui.FAILSAFE = False
+
+sensitivity = 4
+small_movement = 5
+tap_threshold = 0.3
+hand_detection_confidence=0.2
+tracking_confidence=0.1
+
+scroll_mode = True
 
 def distance(a,b):
     return math.hypot(a[0] - b[0], a[1] - b[1])
+
+def midpt(a,b):
+    return ((a[0]+b[0])//2,(a[1]+b[1])//2)
+
+def camera_to_screen(cam_x, cam_y, H):
+    pt = np.array([[[cam_x, cam_y]]], dtype=np.float32)
+    screen_pt = cv2.perspectiveTransform(pt, H)
+    sx, sy = screen_pt[0][0]
+    return int(sx), int(sy)
 
 HAND_CONNECTIONS = [
     (0, 1), (1, 2), (2, 3), (3, 4),      # Thumb
@@ -18,11 +40,12 @@ HAND_CONNECTIONS = [
 
 vid = cv2.VideoCapture(0)
 base_options = python.BaseOptions(model_asset_path='hand_landmarker.task')
-options = vision.HandLandmarkerOptions(base_options=base_options,running_mode=vision.RunningMode.VIDEO,num_hands=1,min_hand_detection_confidence=0.9,min_tracking_confidence=0.9)
+options = vision.HandLandmarkerOptions(base_options=base_options,running_mode=vision.RunningMode.VIDEO,num_hands=1,min_hand_detection_confidence=hand_detection_confidence,min_tracking_confidence=tracking_confidence)
 detector = vision.HandLandmarker.create_from_options(options)
 
-last_contact_time = None
-last_release_time = None
+prev_ti_midpt_y = None
+
+cv2.namedWindow("vid", cv2.WINDOW_NORMAL)
 
 while vid.isOpened():
     try:
@@ -43,22 +66,33 @@ while vid.isOpened():
 
             thumbl = landmarks[4]
             indexl = landmarks[8]
+            middlel = landmarks[12]
             palm_pinky = landmarks[17]
             palm_bottom = landmarks[0]
 
-            fingerdist = distance(thumbl,indexl)
             refrencedist = distance(palm_bottom,palm_pinky)
 
-            ratio = fingerdist/refrencedist
-            if ratio < 0.3:
-                ti_contact = True    # thumb-index contact
-            else:
-                ti_contact = False
-            # print(ratio)
-        # print(result.handedness[0][0].score)
-            cv2.putText(frame, f" Ratio: {ratio}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            # ti -> thumb-index
+            ti_mid = midpt(thumbl,indexl)
+            ti_fingerdist = distance(thumbl,indexl)
+            ti_ratio = ti_fingerdist/refrencedist
+            ti_contact = ti_ratio < tap_threshold    
+
+            if scroll_mode:
+                if ti_contact and prev_ti_midpt_y != None:
+                    diff = ti_mid[1]-prev_ti_midpt_y
+                    if abs(diff) > small_movement:
+                        pyautogui.scroll(int(diff*sensitivity))
+            else: # mouse mode
+                pyautogui.moveTo((indexl[0]-340),(indexl[1]-370))
+                print(indexl)
+                
+            prev_ti_midpt_y = ti_mid[1]
+            
+            cv2.circle(frame, ti_mid, 7, (0,255*(not ti_contact),255*ti_contact), cv2.FILLED)
+
+            cv2.putText(frame, f" Ratio: {ti_ratio}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
             cv2.putText(frame, f"Score: {result.handedness[0][0].score}", (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-            cv2.putText(frame, f" Contact: {ti_contact}", (10, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         cv2.imshow("vid",frame)
     except Exception as e:
         print(e)
